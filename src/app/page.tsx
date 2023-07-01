68e5ea22f9e1980298defa66e61ddd78adde8d6e
maxl85 <maxl85@mail.ru>
2023-07-02 01:51:18 +0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import styles from './page.module.scss';
 export default function Home() {
   return (
     <main className='container'>
-      {/* <h1 className={styles.h}>Hello</h1> */}
+      <h1 className={styles.h}>Hello</h1>
     </main>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,29 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <div className={`container ${styles.wrapper}`}>
-        Header
+        <Link href="/" className={styles.logo}>
+          <Image src="/assets/icons/logo.svg" fill={true} alt="logo" />
+        </Link>
+
+        <nav className={styles.nav}>
+          <ul>
+            <li><Link href="#">меню</Link></li>
+            <li><Link href="#">о нас</Link></li>
+            <li><Link href="#">контакты</Link></li>
+          </ul>
+        </nav>
+
+        <a href="tel:+79184326587" className={styles.phone}>
+          {/* <div className={styles.telephoneIcon}>
+            <Image src="/assets/icons/telephone.svg" fill={true} alt="logo" />
+          </div> */}
+          <Image src="/assets/icons/telephone.svg" width={40} height={40} alt="logo" />
+          <div className={styles.phoneText}>
+            <h4>+7 (918) 432-65-87</h4>
+            <span className="description">Ежедневно с 9:00 до 23:00</span>
+          </div>
+        </a>
+
       </div>
     </header>
   );
