a4ee0a744f5dc94cba1cac4c7e4cc971ed54a76e
maxl85 <maxl85@mail.ru>
2023-07-04 20:37:33 +0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
     {/* <main className='container'> */}
-      {/* <Cover /> */}
+      <Cover />
       <div style={{height: '2500px'}}></div>
     {/* </main> */}
     </>
